102cd25b814abd4d55ca5d0c19e91c613d977ce9
Musthaffa Shaik <musthaffa.shaik@planview.com>
2024-08-08 21:52:39 +0530

    GraphqlApi Added By Musthaffa

---
+++ GraphQLApi/GraphQLApi/Types/AddressType.cs
@@ -0,0 +1,17 @@
+﻿using GraphQLApi.Models;
+
+namespace GraphQLApi.Types
+{
+    public class AddressType :ObjectType<Address>
+    {
+        protected override void Configure(IObjectTypeDescriptor<Address> descriptor)
+        {
+            descriptor.Field(x => x.AddressId).Type<NonNullType<IdType>>();
+            descriptor.Field(x => x.UserId).Type<IdType>();
+            descriptor.Field(x => x.Street).Type<StringType>();
+            descriptor.Field(x => x.City).Type<StringType>();
+            descriptor.Field(x => x.State).Type<StringType>();
+            descriptor.Field(x => x.ZipCode).Type<StringType>();
+        }
+    }
+}
