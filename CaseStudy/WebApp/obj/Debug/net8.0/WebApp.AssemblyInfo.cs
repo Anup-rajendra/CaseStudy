775f2282b02fb8cb344b6d47554534752ce48899
Musthaffa Shaik <musthaffa.shaik@planview.com>
2024-08-12 16:24:31 +0530

    Test Updated

--- CaseStudy/WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ CaseStudy/WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13ed4aad84b6f187222387f259f3bc13646ded59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+861c480f49364ac45d88bcbb99bb42a53902952e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
