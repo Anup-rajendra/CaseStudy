e9699afa7dc432f0cfe433b06abfe41327e98fbd
Musthaffa Shaik <musthaffa.shaik@planview.com>
2024-08-28 16:59:28 +0530

    first commit

---
+++ AdminAccessService/AdminAccessService/Program.cs
@@ -0,0 +1,35 @@
+using AdminAccessService.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<RetailApplicationContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Constr")));
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Set session timeout
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseSession();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Login}/{id?}");
+
+app.Run();
