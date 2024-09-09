e4315d2574a88521c9dfb70c24852bc70b5a178c
Musthaffa Shaik <musthaffa.shaik@planview.com>
2024-09-10 00:49:56 +0530

    notification service added

---
+++ NotificationService/RepositoryLayer/Repo/INotification.cs
@@ -0,0 +1,19 @@
+﻿using RepositoryLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Repo
+{
+    public interface INotification
+    {
+        User getUserByIdAsync(int id);
+
+        Address getAddressByIdAsync(int userid,int num);
+
+        List<Product> getProductsByIdAsync(List<int> productids);
+
+    }
+}
---
+++ NotificationService/RepositoryLayer/Repo/INotificationImplementation.cs
@@ -0,0 +1,50 @@
+﻿using RepositoryLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Repo
+{
+    internal class INotificationImplementation : INotification
+    {
+        RetailApplicationContext _context;
+        public INotificationImplementation( RetailApplicationContext context)
+        {
+            _context = context;
+        }
+        public Address getAddressByIdAsync(int userid, int num)
+        {
+            List<Address> addresses = _context.Addresses.ToList();
+
+            List<Address> useraddresses=new List<Address>();
+
+            foreach (Address address in addresses) {
+                if (address.UserId == userid) {
+                    
+                    useraddresses.Add(address);
+                
+                }
+            }
+            return useraddresses[num];
+        }
+
+        public List<Product> getProductsByIdAsync(List<int>productIds)
+        {
+            List<Product> result=new List<Product>();
+            foreach (int productid in productIds) {
+                Product product = _context.Products.FirstOrDefault(p => p.ProductId == productid);
+                result.Add(product);
+            }
+            
+            return result;
+        }
+
+        public User getUserByIdAsync(int id)
+        {
+            return _context.Users.FirstOrDefault(u => u.UserId == id);
+        }
+    }
+}
